d27ad619ec0a0f0f6a96ea67626ab71639594b42
ossan <ivan.pesenti.dev@gmail.com>
2025-10-08 17:07:59 +0200

    adjusted images

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Become a 10x Developer with Go Performance Optimization
 
 Sample repo to hold code for the session.
+
+[Slides](https://docs.google.com/presentation/d/e/2PACX-1vTWEsuikOaw5YDOxXAHySCrYya2xyZuVnyAoY3nj_IENC-F5ybMGLYEqMH0bteX0mPqqQmug_ysw_iI/pub?start=false&loop=false&delayms=3000)
--- benchmarking/concatenator/string_concat_test.go
+++ benchmarking/concatenator/string_concat_test.go
@@ -11,7 +11,8 @@ var Result string // "try" avoiding compiler optimizations
 func BenchmarkStrings(b *testing.B) {
 	var s string
 	b.ReportAllocs()
-	b.ResetTimer() // you can reset timers/counters for heavy setup
+	// reset timers/counters for heavy setup
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		s = concatenator.Strings("hello", "world")
 	}
@@ -22,7 +23,7 @@ func BenchmarkStrings_1_24(b *testing.B) {
 	b.ReportAllocs()
 	// "b.Loop" tell when to stop
 	for b.Loop() {
-		// this won't be subject to dead code elimination
+		// not subject to dead code elimination
 		concatenator.Strings("hello", "world")
 	}
 }
